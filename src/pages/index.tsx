25e401eee4e4baf33980da76ae6f9dc12f9a613f
damon.chen <chj_0507_dz@sina.com>
2023-05-31 10:38:41 +0800

    fix: 优化切换效果

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,12 +19,6 @@ const useStyles = createStyles(({ token, css }) => ({
     borderRadius: token.borderRadiusLG,
     width: "100vw",
     height: "100vh",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-    marginLeft: "auto",
-    marginRight: "auto",
   },
   card: css`
     color: ${token.colorTextTertiary};
@@ -48,6 +42,7 @@ export default function HomePage() {
   const changePrimaryColor = useSetAtom(primaryColorAtom);
   const { themeMode, appearance, browserPrefers, setThemeMode } =
     useThemeMode();
+  const isDarkMode = themeMode === "dark";
 
   const handle = useMemoizedFn((e: any) => {
     const html = document.querySelector("html")!;
@@ -58,7 +53,7 @@ export default function HomePage() {
       !window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 
     if (!isAppearanceTransition) {
-      setThemeMode(themeMode === "light" ? "dark" : "light");
+      setThemeMode(isDarkMode ? "light" : "dark");
       return;
     }
 
@@ -70,8 +65,8 @@ export default function HomePage() {
 
     // @ts-expect-error: Transition API
     const transition = document.startViewTransition(() => {
-      html.className = themeMode === "light" ? "dark" : "";
-      setThemeMode(themeMode === "light" ? "dark" : "light");
+      html.className = isDarkMode ? "" : "dark";
+      setThemeMode(isDarkMode ? "light" : "dark");
     });
 
     transition.ready.then(() => {
@@ -81,15 +76,14 @@ export default function HomePage() {
       ];
       document.documentElement.animate(
         {
-          clipPath: themeMode === "dark" ? clipPath : [...clipPath].reverse(),
+          clipPath: isDarkMode ? [...clipPath].reverse() : clipPath,
         },
         {
-          duration: 400,
-          easing: "ease-out",
-          pseudoElement:
-            themeMode === "dark"
-              ? "::view-transition-new(root)"
-              : "::view-transition-old(root)",
+          duration: 300,
+          easing: "ease-in",
+          pseudoElement: isDarkMode
+            ? "::view-transition-old(root)"
+            : "::view-transition-new(root)",
         }
       );
     });
@@ -97,7 +91,7 @@ export default function HomePage() {
 
   return (
     <div className={styles.container}>
-      <h2>Yay! Welcome to umi!</h2>
+      <SwitchIcon isDark={isDarkMode} onClick={handle} />
       <div className={styles.card}>
         <Text type={"secondary"}>主题模式：</Text>
         {themeMode}
@@ -106,7 +100,7 @@ export default function HomePage() {
         {appearance}
         <Divider type={"vertical"} />
         <Text type={"secondary"}>浏览器外观：</Text>
-        {browserPrefers}
+        {browserPrefers}S
       </div>
       <Segmented
         value={themeMode}
@@ -117,9 +111,6 @@ export default function HomePage() {
           { label: "暗色", value: "dark" },
         ]}
       />
-      <Button type="primary" onClick={handle}>
-        我是按钮
-      </Button>
       <Form.Item
         valuePropName="color"
         name="colorPrimary"
@@ -130,3 +121,34 @@ export default function HomePage() {
     </div>
   );
 }
+
+const SwitchIcon = ({
+  isDark,
+  onClick,
+}: {
+  isDark: boolean;
+  onClick: (e: any) => void;
+}) => {
+  if (!isDark)
+    return (
+      <div onClick={onClick} style={{ cursor: "pointer" }}>
+        <svg viewBox="0 0 24 24" width="20px" height="20px">
+          <path
+            fill="#000"
+            d="m3.55 19.09l1.41 1.41l1.8-1.79l-1.42-1.42M12 6c-3.31 0-6 2.69-6 6s2.69 6 6 6s6-2.69 6-6c0-3.32-2.69-6-6-6m8 7h3v-2h-3m-2.76 7.71l1.8 1.79l1.41-1.41l-1.79-1.8M20.45 5l-1.41-1.4l-1.8 1.79l1.42 1.42M13 1h-2v3h2M6.76 5.39L4.96 3.6L3.55 5l1.79 1.81l1.42-1.42M1 13h3v-2H1m12 9h-2v3h2"
+          ></path>
+        </svg>
+      </div>
+    );
+
+  return (
+    <div onClick={onClick} style={{ cursor: "pointer" }}>
+      <svg viewBox="0 0 24 24" width="20px" height="20px">
+        <path
+          fill="#fff"
+          d="M2 12a10 10 0 0 0 13 9.54a10 10 0 0 1 0-19.08A10 10 0 0 0 2 12Z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
